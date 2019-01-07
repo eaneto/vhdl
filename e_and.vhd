94483d89a9e7c55eb3395177cfa13e7fabb65809
Edison Neto <ednetoali@gmail.com>
2019-01-06 22:30:07 -0200

    And logic gate

---
+++ e_and.vhd
@@ -0,0 +1,22 @@
+-- Author: Edison Neto
+-- Date: Jan 2019
+
+library IEEE;
+
+use IEEE.std_logic_1164;
+
+entity e_and is
+  port
+  (
+    a : in std_logic;
+    b : in std_logic;
+    o : out std_logic
+  );
+end e_and;
+
+architecture hardware of e_and is
+begin
+
+  o <= a and b;
+
+  end hardware;
